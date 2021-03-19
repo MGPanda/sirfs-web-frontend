ae505a3d6f81f8dd570e8a9623d86e7fc69287d6
Cristóbal Martínez <wipeoman@gmail.com>
2021-03-19 17:29:45 +0100

    Added some base stuff

---
+++ src/core/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Route, Switch} from "react-router-dom";
+import {ClassesPage} from "../../pages/ClassesPage/ClassesPage";
+
+export function Routes() {
+    return(
+        <Switch>
+            <Route path={"/class"}>
+                <ClassesPage/>
+            </Route>
+            <Route exact path={"/"}>
+                <h1>Home</h1>
+            </Route>
+            <Route path={"*"}>
+                <h1>404</h1>
+            </Route>
+        </Switch>
+    )
+}
---
+++ src/pages/ClassesPage/ClassesPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './ClassesPage.scss';
+
+export function ClassesPage() {
+    return(
+        <div className={"p-classes"}>
+
+        </div>
+    )
+}
